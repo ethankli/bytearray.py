543cd28074e14767e2d77c6b597c360e50ced4f0
Ethan Lindley <ethankli@hotmail.com>
2020-12-04 01:22:30 -0600

    amf0: remove useless code from enums

--- src/amf0/enums.py
+++ src/amf0/enums.py
@@ -1,32 +1,6 @@
 from enum import Enum
 
 
-class Sizes(Enum):
-    """
-    Define sizes in bytes of specified data types
-    """
-    INT8_MIN = -128
-    INT8_MAX = 127
-
-    UINT8_MIN = 0
-    UINT8_MAX = 255
-
-    INT16_MIN = -32768
-    INT16_MAX = 32767
-
-    UINT16_MIN = 0
-    UINT16_MAX = 65535
-
-    INT32_MIN = -2147483648
-    INT32_MAX = 2147483647
-
-    UINT32_MIN = 0
-    UINT32_MAX = 4294967295
-
-    def __str__(self):
-        return int(self.value)
-
-
 class Markers(Enum):
     """
     Hexadecimal markers for specified data types
@@ -49,14 +23,6 @@ class Markers(Enum):
     XML_DOCUMENT = '\x0F'  # Not supported
     TYPED_OBJECT = '\x10'
     AVMPLUS = '\x11'  # Not supported
-
-
-class ObjectEncodings(Enum):
-    """
-    Identify object encodings
-    """
-    AMF0 = 0
-    AMF3 = 3
 
 
 class ByteOrder(Enum):
