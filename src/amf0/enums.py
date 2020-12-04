ce069c82b733f8f66efbdcc52306ed6e0e29ea58
Ethan Lindley <ethankli@hotmail.com>
2020-12-04 01:58:08 -0600

    amf0: restore sizes enum, support writing data

--- src/amf0/enums.py
+++ src/amf0/enums.py
@@ -1,6 +1,32 @@
 from enum import Enum
 
 
+class Sizes(Enum):
+    """
+    Define sizes in bytes of specified data types
+    """
+    INT8_MIN = -128
+    INT8_MAX = 127
+
+    UINT8_MIN = 0
+    UINT8_MAX = 255
+
+    INT16_MIN = -32768
+    INT16_MAX = 32767
+
+    UINT16_MIN = 0
+    UINT16_MAX = 65535
+
+    INT32_MIN = -2147483648
+    INT32_MAX = 2147483647
+
+    UINT32_MIN = 0
+    UINT32_MAX = 4294967295
+
+    def __str__(self):
+        return int(self.value)
+
+
 class Markers(Enum):
     """
     Hexadecimal markers for specified data types
--- src/amf0/stream.py
+++ src/amf0/stream.py
@@ -1,6 +1,7 @@
 import struct
 
 from .enums import ByteOrder
+from .enums import Sizes
 
 
 class Stream(object):
@@ -8,7 +9,7 @@ class Stream(object):
     Reads and writes binary data types for use with serialization and/or deserialization
     """
     
-    def __init__(self, data=list, endian=ByteOrder.BE):
+    def __init__(self, data=list(), endian=ByteOrder.BE):
         self._data = data  # Store the data
         self._position = 0  # Store the position in the data stream
         self._endian = endian.value  # Identify the byte order for the given data
@@ -64,3 +65,32 @@ class Stream(object):
         value = struct.unpack(''.join([self._endian, 'H']), self._data[self._position])
         self._position += 1
         return value[0]
+
+    def write_boolean(self, value: bool):
+        self._data.append(struct.pack(''.join([self._endian, '?']), value))
+
+    def write_byte(self, value: bytes):
+        if len(value) == 1:
+            self._data.append(struct.pack(''.join([self._endian, 'c']), value))
+
+    def write_double(self, value: float):
+        self._data.append(struct.pack(''.join([self._endian, 'd']), value))
+
+    def write_float(self, value: float):
+        self._data.append(struct.pack(''.join([self._endian, 'f']), value))
+
+    def write_int(self, value: int):
+        if value <= Sizes.INT32_MAX.value and value >= Sizes.INT32_MIN.value:
+            self._data.append(struct.pack(''.join([self._endian, 'i']), value))
+
+    def write_uint(self, value: int):
+        if value <= Sizes.UINT32_MAX.value and value >= Sizes.UINT32_MIN.value:
+            self._data.append(struct.pack(''.join([self._endian, 'I']), value))
+
+    def write_short(self, value: int):
+        if value <= Sizes.INT16_MAX.value and value >= Sizes.INT16_MIN.value:
+            self._data.append(struct.pack(''.join([self._endian, 'h']), value))
+        
+    def write_ushort(self, value: int):
+        if value <= Sizes.UINT16_MAX.value and value >= Sizes.UINT16_MIN.value:
+            self._data.append(struct.pack(''.join([self._endian, 'H']), value))
